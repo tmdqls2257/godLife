c8e752a3560832794ae7cbf8d5b44f01c0523beb
tmdqls2257 <tmdqls2257@gmail.com>
2022-11-04 00:29:50 +0900

    💄 add bottomnavigation

---
+++ lib/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:god_life/shared/theme.dart';
+
+import 'Routine/Routine_view.dart';
+import 'exercise/exercise_view.dart';
+import 'meeting/meeting_view.dart';
+import 'shared/shared.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIdx = 0;
+  List<Widget> listWidget = [
+    const Meeting(),
+    const Routine(),
+    const Exercise(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: listWidget[_currentIdx],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            label: 'meeting',
+            icon: svgIcon[IconType.home] as Widget,
+            activeIcon: svgIcon[IconType.homeOrange] as Widget,
+          ),
+          BottomNavigationBarItem(
+            label: 'routine',
+            icon: svgIcon[IconType.calendar] as Widget,
+            activeIcon: svgIcon[IconType.calendarOrange] as Widget,
+          ),
+          BottomNavigationBarItem(
+            label: 'exercise',
+            icon: svgIcon[IconType.exercise] as Widget,
+            activeIcon: svgIcon[IconType.exerciseOrange] as Widget,
+          ),
+        ],
+        selectedItemColor: customColor[ColorType.orange],
+        currentIndex: _currentIdx,
+        onTap: (value) => setState(
+          () {
+            _currentIdx = value;
+          },
+        ),
+      ),
+    );
+  }
+}
