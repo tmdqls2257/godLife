7924edaba8ec5431fdd09730b34c1e72cbaef8a0
tmdqls2257 <tmdqls2257@gmail.com>
2022-11-08 13:09:08 +0900

    🔥 remove unused import

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:god_life/shared/theme.dart';
 
 import 'Routine/Routine_view.dart';
 import 'exercise/exercise_view.dart';
